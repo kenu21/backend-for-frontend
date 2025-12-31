6414d2ee7e232478543329ecfc3c1222f8e6c473
Yurii <yurij.kenu@gmail.com>
2025-12-31 14:48:21 +0100

    Initial commit

---
+++ src/test/java/com/keniu/backend_for_frontend/BackendForFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keniu.backend_for_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendForFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
